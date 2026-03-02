64faf1227ae66b2723af7c109fbb8f5a9744d508
aslamsm <aslamsm@yahoo.com>
2026-03-02 23:21:33 +0300

    app.tsx updated

--- client/App.tsx
+++ client/App.tsx
@@ -346,6 +346,7 @@ function App() {
         >
           <Routes>
             <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Navigate to="/home" replace />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/users" element={<UserList2 />} />
             <Route path="/users/:id" element={<UserDetails />} />
