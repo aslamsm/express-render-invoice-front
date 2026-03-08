52b7e6a9de27298bb569b12b63d490580412f4f3
aslamsm <aslamsm@yahoo.com>
2026-03-08 16:18:11 +0530

    update port

--- client/Components/CreateCustomer.tsx
+++ client/Components/CreateCustomer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
 const API_URL = "http://localhost:3000/customers";
